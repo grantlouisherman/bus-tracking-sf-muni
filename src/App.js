26476b3a81439c3adc6851c3eb8ed4b95fe55578
grantlouisherman <grantlouisherman041@gmail.com>
2020-07-25 10:14:30 -0400

    got real time info for bus locations

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
 import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import { Marker } from "google-maps-react";
-import { fetchRoutes, fetchRouteTag } from './utils';
+import { fetchRoutes, fetchVehicleLocation, getMarkersForVehicles } from './utils';
 
 import MapContainer from './components/Map';
 
 const App = () => {
   const [ markers, setMarkers ] = useState([]);
+  const [ tags, setTags ] = useState([]);
   useEffect( async () => {
     const routeConfigResponse = await fetchRoutes();
     const routeConfig = await routeConfigResponse.json();
     const tags = Object.keys(routeConfig.route)
     .map(routeKey => routeConfig.route[routeKey].tag);
     const routes = await Promise.all(tags.map(async (tag) => {
-      const routeData = await fetchRouteTag(tag);
+      const routeData = await fetchVehicleLocation(tag, 0);
       return routeData.json();
     }));
     setMarkers(routes)
-  }, [])
+    setTags(tags);
+  }, []);
 
-  console.log(markers)
+  setTimeout(async () => {
+    const routes = await Promise.all(tags.map(async (tag) => {
+      const routeData = await fetchVehicleLocation(tag, 0);
+      return routeData.json();
+    }));
+    setMarkers(routes)
+   }, 15000);
+   
   return (
     <div className="App">
       <MapContainer>
-      {markers && markers.map(({ route: { latMin, lonMin, tag}}) => (
-        <Marker key={tag} position={{lat: latMin, lng: lonMin}} />
-      ))}
+      { markers && markers.map(({vehicle}) => vehicle && getMarkersForVehicles(vehicle))}
       </MapContainer>
+
     </div>
   )
 }
 
 export default App;
+
+// {markers && markers.map(({ route: { latMin, lonMin, tag}}) => (
+//   <Marker key={tag} position={{lat: latMin, lng: lonMin}} />
+// ))}
+// </MapContainer>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import { Marker } from "google-maps-react";
 
-export const fetchRoutes = async () => await fetch('http://webservices.nextbus.com/service/publicJSONFeed?command=routeList&a=sf-muni')
-export const fetchRouteTag = async (routeTag) => await fetch(`http://webservices.nextbus.com/service/publicJSONFeed?command=routeConfig&a=sf-muni&r=${routeTag}`)
+export const fetchRoutes = async () => await fetch('http://webservices.nextbus.com/service/publicJSONFeed?command=routeList&a=sf-muni');
+export const fetchVehicleLocation = async (routeTag) => await fetch(`http://webservices.nextbus.com/service/publicJSONFeed?command=vehicleLocations&a=sf-muni&r=${routeTag}&t=0`);
+
+export const getMarkersForVehicles = (vehicle) =>
+vehicle.map(({ id , lat, lon }) => <Marker key={id} position={{lat, lng:lon}} /> );
